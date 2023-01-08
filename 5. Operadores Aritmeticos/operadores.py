f65375811a81bcf2b4aa2d56e86da1251241fe18
Brandon12536 <bp754509@gmail.com>
2023-01-08 17:49:10 -0600

    Operadores Aritméticos

---
+++ 5. Operadores Aritmeticos/operadores.py
@@ -0,0 +1,18 @@
+int = num1 = 10
+int = num2 = 5
+
+#Suma
+print (num1 + num2)
+#Resta
+print (num1 - num2)
+#Multiplicacion
+print (num1 * num2)
+#Exponente
+print (num1 ** num2)
+#División
+print (num1 / num2)#Divide pero toma el resultado con decimal
+#Division entera
+print (num1 // num2) #se toma el numero entero
+#Módulo
+print (num1 % num2)#Módulo es el residuo de la division entera
+
