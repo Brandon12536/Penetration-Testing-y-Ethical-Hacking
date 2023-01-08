d6157e97b4190ad138716b1b50be354aeb686dbf
Brandon12536 <bp754509@gmail.com>
2023-01-08 17:51:11 -0600

    Entrada de datos

---
+++ 9. Entrada de datos/I/O.py
@@ -0,0 +1,9 @@
+nombre =  input("Por favor ingrese su nombre\n>>") #Entrada de datos en consola
+print ("Hola " + nombre) #imprime el valor guardado en la variable nombre
+
+numero1 = int(input ("Ingrese un número\n>>")) #casteo
+print(5 + numero1)
+
+numero1 = int(input("Ingrese el primer digito\n>>"))
+numero2 = int(input("Ingrese el segundo digito\n>>"))
+print(numero1 + numero2)
