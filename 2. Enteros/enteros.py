6f0a6fe5195d3763df9084b008bec8b4ad7f7c4a
Brandon12536 <bp754509@gmail.com>
2023-01-08 17:47:26 -0600

    Números enteros

---
+++ 2. Enteros/enteros.py
@@ -0,0 +1,8 @@
+numero = 10 #numero entero
+num = "8" #valor cadena
+nu = int("6") #Casteo define que tipo de dato es el que se esta ingresando y lo transforma a entero
+print (numero)#valor entero
+print (type(num))# devuelve el tipo de valor, si es cadena o entero
+print (nu)
+
+
