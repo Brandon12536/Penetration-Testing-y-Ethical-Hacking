3b72b9f92aa2b17caeb6e8ddb3985152b9d1962c
Brandon12536 <bp754509@gmail.com>
2023-01-08 17:50:18 -0600

    Operadores Relacionales

---
+++ 7. Operadores relacionales/operadores.py
@@ -0,0 +1,21 @@
+num = 10
+num1 = 11
+Expresion = num == num1#Comparacion de ambos numeros para saber si son iguales
+Expresion1 = num > num1#Comparacion para saber si es mayor el numero
+Expresion2 = num < num1#Para saber si es menor el numero
+Expresion3 = num >= num1#Si es mayor o igual que
+Expresion4 = num <= num1#Si es menos o igual que
+Expresion5 = num != num1#Si es diferente del segundo numero
+
+
+print (Expresion)
+print (Expresion1)
+print (Expresion2)
+print (Expresion3)
+print (Expresion4)
+print (Expresion5)
+
+
+nombre = "Hola".lower() == "hola"#Lower para que no tome en cuenta si es mayuscula o minuscula,
+# mientras sea la misma cadena devuelve true, en caso contrario, devolveria false
+print(nombre)
