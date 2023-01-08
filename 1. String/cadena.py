669bf7cb838a21fde48a811627e90d9949390b4a
Brandon12536 <bp754509@gmail.com>
2023-01-08 17:46:55 -0600

    Cadenas

---
+++ 1. String/cadena.py
@@ -0,0 +1,8 @@
+nombre = "brandon".upper() #Transforma el texto a mayusculas
+nombre = "Brandon"#Cadena de texto
+print (nombre[0])#toma el valor que esta en la cadena nombre en la posición 0, 1, 2, 3
+print (len (nombre))#retorna el tamaño de la cadena ejemplo Ana tiene tamaño de 3 ya que son 3 letras 
+print (nombre[0:8])#Retorna la cadena completa el 8 corresponde al ultimo valor
+print (nombre[-1])#Retorna ultima posicion de la cadena ya que es posicion inversa
+print (nombre.isupper())#Verifica si esta en mayusculas y lo transforma
+print (nombre.islower())#Verifica si esta en minusculas y lo transforma
